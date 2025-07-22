25a89fc15b58ccb49b4401361e95276ef76cf29f
Panseriya <hardik.panseriya@hotmail.com>
2025-07-22 11:38:40 +0530

    animations update

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,11 +1,35 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { rightImg, watchImg } from '../utils'
+import VideoCarousel from './VideoCarousel'
+
+// Register ScrollTrigger
+gsap.registerPlugin(ScrollTrigger)
 
 const Highlights = () => {
   useGSAP(() => {
-    gsap.to('#title', { opacity: 1, y: 0 })
-    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
+    gsap.to('#title', {
+      opacity: 1,
+      y: 0,
+      scrollTrigger: {
+        trigger: '#title',
+        start: 'top 80%',
+        toggleActions: 'play none none reverse'
+      }
+    })
+
+    gsap.to('.link', {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
+      scrollTrigger: {
+        trigger: '.link',
+        start: 'top 85%',
+        toggleActions: 'play none none reverse'
+      }
+    })
   }, [])
 
   return (
@@ -15,21 +39,23 @@ const Highlights = () => {
     >
       <div className='screen-max-width'>
         <div className='mb-12 w-full md:flex items-end justify-between'>
-          <h1 id='title' className='section-heading'>
+          <h1 id='title' className='section-heading opacity-0 translate-y-10'>
             Get the highlights.
           </h1>
 
           <div className='flex flex-wrap items-end gap-5'>
-            <p className='link'>
+            <p className='link opacity-0 translate-y-10'>
               Watch the film
               <img src={watchImg} alt='watch' className='ml-2' />
             </p>
-            <p className='link'>
+            <p className='link opacity-0 translate-y-10'>
               Watch the event
               <img src={rightImg} alt='right' className='ml-2' />
             </p>
           </div>
         </div>
+
+        <VideoCarousel />
       </div>
     </section>
   )
