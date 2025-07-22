961145e8d76cae87aae014c90be20fa92fdbf295
Panseriya <hardik.panseriya@hotmail.com>
2025-07-22 12:28:51 +0530

    some changes

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,56 +1,56 @@
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { rightImg, watchImg } from '../utils'
-import VideoCarousel from './VideoCarousel'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 // Register ScrollTrigger
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const Highlights = () => {
   useGSAP(() => {
-    gsap.to('#title', {
+    gsap.to("#title", {
       opacity: 1,
       y: 0,
       scrollTrigger: {
-        trigger: '#title',
-        start: 'top 80%',
-        toggleActions: 'play none none reverse'
-      }
-    })
+        trigger: "#title",
+        start: "top 80%",
+        toggleActions: "restart none none restart",
+      },
+    });
 
-    gsap.to('.link', {
+    gsap.to(".link", {
       opacity: 1,
       y: 0,
       duration: 1,
       stagger: 0.25,
       scrollTrigger: {
-        trigger: '.link',
-        start: 'top 85%',
-        toggleActions: 'play none none reverse'
-      }
-    })
-  }, [])
+        trigger: ".link",
+        start: "top 85%",
+        toggleActions: "restart none none restart",
+      },
+    });
+  }, []);
 
   return (
     <section
-      id='highlights'
-      className='w-screen overflow-hidden h-full common-padding bg-zinc'
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
     >
-      <div className='screen-max-width'>
-        <div className='mb-12 w-full md:flex items-end justify-between'>
-          <h1 id='title' className='section-heading opacity-0 translate-y-10'>
+      <div className="screen-max-width">
+        <div className="mb-12 w-full md:flex max-sm:items-center items-end justify-between">
+          <h1 id="title" className="section-heading opacity-0 translate-y-10">
             Get the highlights.
           </h1>
 
-          <div className='flex flex-wrap items-end gap-5'>
-            <p className='link opacity-0 translate-y-10'>
+          <div className="flex flex-wrap items-end gap-5 max-sm:gap-3">
+            <p className="link opacity-0 translate-y-10">
               Watch the film
-              <img src={watchImg} alt='watch' className='ml-2' />
+              <img src={watchImg} alt="watch" className="ml-2" />
             </p>
-            <p className='link opacity-0 translate-y-10'>
+            <p className="link opacity-0 translate-y-10">
               Watch the event
-              <img src={rightImg} alt='right' className='ml-2' />
+              <img src={rightImg} alt="right" className="ml-2" />
             </p>
           </div>
         </div>
@@ -58,7 +58,7 @@ const Highlights = () => {
         <VideoCarousel />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Highlights
+export default Highlights;
--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,66 +1,25 @@
-import { useRef, useState } from 'react'
-import { hightlightsSlides } from '../constants'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-
-export default function VideoCarousel () {
-  const videoRef = useRef([])
-  const videoSpanRef = useRef([])
-  const videoDivRef = useRef([])
-
-  const [video, setVideo] = useState({
-    isEnd: false,
-    startPlay: false,
-    videoId: 0,
-    isLastVideo: false,
-    isPlaying: false
-  })
-
-  const [loadedData, setLoadedData] = useState([])
-  const { isEnd, isLastVideo, startPlay, videoId, isPlaying } = video
-
-  useGSAP(()=>{
-    gsap.to('#video', {
-        scrollTrigger: {
-            trigger:'#video',
-            toggleActions:'restart'
-        },
-      onComplete: () => {
-          setVideo((pre) => ({
-              ...pre,
-              startPlay: true,
-              isPlaying: true
-          }));
-      }
-    });
-  }, [isEnd, videoId])
+import { hightlightsSlides } from "../constants";
+export default function VideoCarousel() {
   return (
     <>
-      <div className='flex items-center'>
-        {hightlightsSlides.map((list, i) => (
-          <div key={list.id} id='slider' className='sm:pr-20 pr-10'>
-            <div className='video-carousel_container'>
-              <div
-                className='w-full h-full 
-            flex-center rounded-3xl bg-black overflow-hidden'
-              >
-                <video id='video' preload='auto' muted 
-                playsInline={true} 
-                ref={(el)=> (videoRef.current[i] = el)}>
-                  <source src={list.video} type='video/mp4' />
-                </video>
-              </div>
-              <div className='absolute top-12 left-[5%] z-10'>
-                {list.textLists.map((text, i) => (
-                  <p key={i} className='md:text-2xl text-xl font-medium'>
-                    {text}
-                  </p>
-                ))}
+      <div className="flex items-center">
+        {hightlightsSlides?.map((list, i) => {
+          return (
+            <div key={i} id="slider" className="sm:pr-20 pr-10">
+              <div className="video-carousel_container">
+                <div
+                  className="w-full h-full flex-center rounded-3xl overflow-hidden
+                  bg-black"
+                >
+                  <video>
+                    <source src={list.video} type="video/mp4" />
+                  </video>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </>
-  )
+  );
 }
