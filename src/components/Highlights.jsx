eeeb0948ac507b1433f7a3b43430eb67bf439072
Panseriya <hardik.panseriya@hotmail.com>
2025-07-22 11:28:45 +0530

    code formatting

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,13 +1,13 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { rightImg, watchImg } from '../utils'
-import VideoCourse from './VideoCarousel'
 
 const Highlights = () => {
   useGSAP(() => {
     gsap.to('#title', { opacity: 1, y: 0 })
     gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
   }, [])
+
   return (
     <section
       id='highlights'
@@ -16,17 +16,18 @@ const Highlights = () => {
       <div className='screen-max-width'>
         <div className='mb-12 w-full md:flex items-end justify-between'>
           <h1 id='title' className='section-heading'>
-            Get the highlights
+            Get the highlights.
           </h1>
+
           <div className='flex flex-wrap items-end gap-5'>
-            <div className='flex items-center'>
-              <span className='link'>Watch the film</span>
-              <img src={watchImg} alt='watch film' className='ml-2 link' />
-            </div>
-            <div className='flex items-center'>
-              <span className='link'>Watch the event</span>
-              <img src={rightImg} alt='watch event' className='ml-2 link' />
-            </div>
+            <p className='link'>
+              Watch the film
+              <img src={watchImg} alt='watch' className='ml-2' />
+            </p>
+            <p className='link'>
+              Watch the event
+              <img src={rightImg} alt='right' className='ml-2' />
+            </p>
           </div>
         </div>
       </div>
