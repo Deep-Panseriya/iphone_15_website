a5dec88daa857647deaddc1d1af6175f7eaea8eb
Panseriya <hardik.panseriya@hotmail.com>
2025-07-22 11:23:40 +0530

    code format

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
-import { appleImg, bagImg, searchImg } from "../utils";
-import { navLists } from "../constants";
+import { appleImg, bagImg, searchImg } from '../utils'
+import { navLists } from '../constants'
 
 const Navbar = () => {
   return (
     <header
-      className="w-full py-5 sm:px-10 px-5 flex 
-    justify-between items-center"
+      className='w-full py-5 sm:px-10 px-5 flex 
+    justify-between items-center'
     >
-      <nav className="w-full flex screen-max-width">
+      <nav className='w-full flex screen-max-width'>
         <img
           src={appleImg}
-          alt="apple"
+          alt='apple'
           width={14}
           height={18}
-          className="cursor-pointer"
+          className='cursor-pointer'
         />
-        <div className="flex flex-1 justify-center max-sm:hidden">
-          {navLists?.map((nav) => (
+        <div className='flex flex-1 justify-center max-sm:hidden'>
+          {navLists?.map(nav => (
             <div
               key={nav}
-              className="px-5 text-sm cursor-pointer
-            text-gray hover:text-white"
+              className='px-5 text-sm cursor-pointer
+            text-gray hover:text-white'
             >
               {nav}
             </div>
@@ -28,27 +28,27 @@ const Navbar = () => {
         </div>
 
         <div
-          className="flex justify-center 
-        items-baseline gap-7 max-sm:flex-1 max-sm:justify-end"
+          className='flex justify-center 
+        items-baseline gap-7 max-sm:flex-1 max-sm:justify-end'
         >
           <img
             src={searchImg}
-            alt="search"
+            alt='search'
             width={18}
             height={18}
-            className="cursor-pointer"
+            className='cursor-pointer'
           />
           <img
             src={bagImg}
-            alt="bag"
+            alt='bag'
             width={18}
             height={18}
-            className="cursor-pointer"
+            className='cursor-pointer'
           />
         </div>
       </nav>
     </header>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
